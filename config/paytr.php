f05651a18458d58bc457f0e3003b4b6349be1ff4
cartel <119452317+cartelfx@users.noreply.github.com>
2024-09-11 22:16:03 +0400

    Create paytr.php

---
+++ config/paytr.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'merchant_id' => '789368193781', //merchant id
+    'merchant_key' => '', // merchant key
+    'merchant_salt' => '', // merchant salt
+    'test_mode' => false,
+    'debug_on' => true,
+    'currency' => 'TL', // Para birimi
+    'ok_url' => 'https://cartelfx.live/paytr/odeme-basarili',
+    'fail_url' => 'https://cartelfx.live/paytr/odeme-basarisiz',
+];
